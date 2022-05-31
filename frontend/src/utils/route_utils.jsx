6fad0c8b9bdf31a06640ef6f21e5fdd690ef46f2
Billy <billy.phan4@gmail.com>
2022-05-31 10:35:03 -0700

    add protected routes

--- frontend/src/utils/route_utils.jsx
+++ frontend/src/utils/route_utils.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Redirect, Route, withRouter } from 'react-router-dom'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
 
-const mSTP = state => ({
-    loggedIn: Boolean(state.session.currentUser)
-})
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route path={path} exact={exact} render={(props) => (
+    !loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/" />
+    )
+  )} />
+);
 
-const Auth = ({loggedIn, path, component: Component }) => (
-    <Route
-        path={path}
-        render={props => (
-            loggedIn ? <Redirect to='/' /> : <Component {...props} />
-        )}
-    />
-)
+const Protected = ({ component: Component, loggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      loggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
-export const AuthRoute = withRouter(connect(mSTP)(Auth))
+const mapStateToProps = state => (
+  {loggedIn: state.session.isAuthenticated}
+);
 
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
