c96351ba5db740b0a9664985a48911d80aa65f21
Billy <billy.phan4@gmail.com>
2022-05-31 09:32:40 -0700

    add front-end user

--- frontend/src/utils/session.js
+++ frontend/src/utils/session.js
@@ -1,23 +1,43 @@
-export const postUser = user => (
-    $.ajax({
-        url: '/api/users',
-        method: 'POST',
-        data: {user}
-    })
+import axios from "axios";
+
+export const setAuthToken = token => {
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+    } else {
+      delete axios.defaults.headers.common['Authorization'];
+    }
+  };
+
+  
+
+export const signup = userData => (
+    axios.post('/api/users/register', userData)
 )
 
-export const postSession = user => (
-    $.ajax({
-        url: 'api/session',
-        method: 'POST',
-        data: { user }
-    })
+export const login = userData => (
+    axios.post('/api/users/login', userData)
 )
 
+// export const postUser = user => (
+//     $.ajax({
+//         url: '/api/users',
+//         method: 'POST',
+//         data: {user}
+//     })
+// )
 
-export const deleteSession = () => (
-    $.ajax({
-        url: '/api/session',
-        method: 'DELETE'
-    })
-)
+// export const postSession = user => (
+//     $.ajax({
+//         url: 'api/session',
+//         method: 'POST',
+//         data: { user }
+//     })
+// )
+
+
+// export const deleteSession = () => (
+//     $.ajax({
+//         url: '/api/session',
+//         method: 'DELETE'
+//     })
+// )
