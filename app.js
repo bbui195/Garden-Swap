726f8d3bd2f009e70e7d5290aaff826f98d31ee2
AA Student <appacademy@Mini-41.local>
2022-05-31 10:06:28 -0700

    Add user table

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const bodyParser = require('body-parser');
-// const passport = require("passport");
+const passport = require("passport");
 
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -10,13 +10,15 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+const users = require("./routes/api/users");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-// app.use(passport.initialize());
-// require('./config/passport')(passport);
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use("/api/users", users);
 
-// app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
