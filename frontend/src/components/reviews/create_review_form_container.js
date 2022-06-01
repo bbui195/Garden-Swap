79836f7c8b2790a7bbc6a7db636266c46bda102d
Billy <billy.phan4@gmail.com>
2022-06-01 14:23:53 -0700

    create review form container

---
+++ frontend/src/components/reviews/create_review_form_container.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux'
+import ReviewForm from './leave_review_form'
+import { makeReview } from '../../actions/review_action'
+
+
+const mSTP = (state, ownProps) => {
+    return {
+        formType: "Create Review",
+        fullName: state.session.currentUser.name,
+        review : {
+            product_id: ownProps.match.params.userId,
+            comment: '',
+            rating: '',
+        }
+    }
+}
+
+const mDTP = dispatch => {
+    return {
+            action: review => dispatch(makeReview(review))
+    }
+}
+
+export default connect(mSTP, mDTP)(ReviewForm)
