9ffe2cb0895b867692a512c8e12c00b9cc298a22
Cole Martindale <cole.martindale13@gmail.com>
2022-06-01 13:47:49 -0700

    Resize prof pic

--- frontend/src/components/navbar/nav_bar.jsx
+++ frontend/src/components/navbar/nav_bar.jsx
@@ -2,18 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import words from "../../assets/images/words.png";
 import cabbage from "../../assets/images/cabbage.png";
-// import { FaLocationArrow } from "react-icons/Fa";
 import profilePic from "../../assets/images/cabbage.jpeg"
 
-export default ({ currentUser,logoutUser}) => {
+export default ({ currentUser, logoutUser}) => {
 
     const session = currentUser ? (
         <div>
             <div>
-                <img src={profilePic} alt="" />  
+                <img src={profilePic} alt="prof" className='prof'/>  
             </div>
             <button onClick={logoutUser}>Log Out</button>
         </div>
+        // <div className="profile-dropdown" onClick={this.toggleDropDown}>
+        //     <img src={window.profilePicLogo} className='profile-logo' />
+        //     <img src={window.downArrowIcon} className='down-arrow-icon' />
+        //     <div className='dropdown-content'>
+        //         <div className='email-container'>
+        //             <img src={window.profilePicLogo} className='profile-logo' />
+        //             <span>{this.props.currentUser.username}</span>
+        //         </div>
+        //         <div className='orders-container'>
+        //             <img src={window.ordersIcon} className='orders-icon' />
+        //             <Link to='/orders'>Order History</Link>
+        //         </div>
+        //         <div className='sign-out-container'>
+        //             <img src={window.signOutIcon} className='sign-out-icon' />
+        //             <button className="logout-button" onClick={this.props.logout}>Log Out</button>
+        //         </div>
+        //     </div>
+        // </div>
     ) : (
         <>
             <ul className='container'>
