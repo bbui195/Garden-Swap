5390081b4feb0c3281c303695df7470d6b7db48f
John Gardner <john.s.gardner@gmail.com>
2022-06-01 14:53:38 -0700

    Prepare for seeding

--- frontend/src/components/listings/listing_form.jsx
+++ frontend/src/components/listings/listing_form.jsx
@@ -15,7 +15,7 @@ class ListingForm extends React.Component {
         const file = e.currentTarget.files[0];
         const fileReader = new FileReader();
         fileReader.onloadend = function(){
-            this.setState({imageFile: file, photoUrls: fileReader.result})
+            this.setState({imageFile: file, photoUrls: [...this.state.photoUrls, fileReader.result]})
         }.bind(this)
         if (file){
             fileReader.readAsDataURL(file);
