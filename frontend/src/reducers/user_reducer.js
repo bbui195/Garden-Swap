735ba38b987be8f667d7082b06eff7c0a4f6390d
Billy <billy.phan4@gmail.com>
2022-05-31 17:43:12 -0700

    product show

--- frontend/src/reducers/user_reducer.js
+++ frontend/src/reducers/user_reducer.js
@@ -1,11 +1,11 @@
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_USER } from "../actions/user_action";
 
 const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState)
     let nextState = Object.assign({}, oldState)
     switch (action.type) {
-        case RECEIVE_CURRENT_USER:
-            nextState[action.username] = action.username
+        case RECEIVE_USER:
+            nextState[action.username] = action.data
             return nextState
         default:
             return oldState;
