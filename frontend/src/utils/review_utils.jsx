0244d224ea906d5a58357715e3afdc0ab79b8989
Billy <billy.phan4@gmail.com>
2022-06-01 11:13:43 -0700

    review action/reducer/utils

---
+++ frontend/src/utils/review_utils.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const fetchReviews = () => {
+    return axios.get('api/reviews')
+}
+
+export const fetchReview = reviewId => {
+    return axios.get(`/api/reviews/${reviewId}`)
+}
+
+export const createReview = review => {
+    return axios.post('/api/users/review',review)
+}
+
+export const deleteReview = reviewId => {
+    axios.delete(`/api/reviews/${reviewId}`)
+}
+
+export const patchReview = review => {
+    return axios.patch(`/api/reviews/${review.id}`,review)
+}
+
